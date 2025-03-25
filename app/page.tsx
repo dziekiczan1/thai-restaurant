41c2927aee700f38a6b4b3f48809987f576331fb
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-03-25 19:31:15 +0100

    add custom fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-  return <h1>Thai restaurant!</h1>
+  return (
+    <div>
+      <h1 className="font-grotesk">Cabinet Grotesk</h1>
+      <p className="font-chillax">Chillax Text</p>
+      <h2 className="font-bitter text-primary">Bitter Rose</h2>
+    </div>
+  );
 }
