c2e17c80b024de848b4f0faeabb317babb2ee66f
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-04-02 20:26:08 +0200

    adjustments for mobile display

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,9 @@
 export const Header = () => {
   return (
     <header
-      className={`flex justify-center items-center pt-16 relative w-full`}
+      className={`flex justify-center items-center pt-4 lg:pt-16 relative w-full`}
     >
-      <h1 className="font-chillax text-white text-4xl">chantara</h1>
+      <h1 className="font-chillax text-white text-xl lg:text-4xl">chantara</h1>
     </header>
   );
 };
--- components/section-info.tsx
+++ components/section-info.tsx
@@ -11,11 +11,13 @@ export const SectionInfo = ({
 }: SectionInfoProps) => {
   return (
     <div className="flex flex-col justify-center items-center w-full flex-1 relative">
-      <p className="font-bitter text-primary text-7xl">{subtitle}</p>
-      <h2 className="font-chillax text-white text-9xl font-medium tracking-wide">
+      <p className="font-bitter text-primary text-4xl lg:text-7xl">
+        {subtitle}
+      </p>
+      <h2 className="font-chillax text-white text-4xl lg:text-9xl font-medium tracking-wide">
         {title}
       </h2>
-      <p className="font-chillax text-center text-white text-2xl/10 font-light mt-8 max-w-xl">
+      <p className="font-chillax text-center text-white text-2xl/10 font-light mt-8 max-w-xl hidden lg:block">
         {description}
       </p>
     </div>
